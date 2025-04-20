cdb6ad47a361ed46867d491029b0b574a184d3ff
violet <v.violette.yy@gmail.com>
2025-04-20 15:24:25 +0800

    crosshair !!! meow

--- src/main/java/org/violetyy/projectileweapontimer/ModClient.java
+++ src/main/java/org/violetyy/projectileweapontimer/ModClient.java
@@ -1,6 +1,7 @@
 package org.violetyy.projectileweapontimer;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.sound.SoundCategory;
@@ -8,6 +9,8 @@ import net.minecraft.sound.SoundEvents;
 import net.minecraft.text.Text;
 import me.shedaniel.autoconfig.AutoConfig;
 import me.shedaniel.autoconfig.serializer.Toml4jConfigSerializer;
+import net.minecraft.util.math.ColorHelper;
+import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.Vec3d;
 import org.jetbrains.annotations.Nullable;
 
@@ -21,8 +24,22 @@ public class ModClient implements ClientModInitializer {
 
 	private boolean finishedCharging = false;
 
+	public static int actualTicksElapsed = 0;
+
+
 	@Override
 	public void onInitializeClient() {
+
+		ClientTickEvents.END_CLIENT_TICK.register(client -> {
+			if (client.player instanceof ProjectileWeaponTimerUtils access) {
+				if (access.templateProject$getHeldTicks() != 0) {
+					actualTicksElapsed++;
+				} else {
+					actualTicksElapsed = 0;
+				}
+			}
+		});
+
 
 		AutoConfig.register(ModConfig.class, Toml4jConfigSerializer::new);
 		config = AutoConfig.getConfigHolder(ModConfig.class).getConfig();
@@ -67,6 +84,57 @@ public class ModClient implements ClientModInitializer {
 				}
 			}
 		});
+
+		// crosshair
+
+		HudRenderCallback.EVENT.register((context, tickDelta) -> {
+
+			int centerX = (context.getScaledWindowWidth() - 1) / 2;
+			int centerY = (context.getScaledWindowHeight() - 1) / 2;
+
+			MinecraftClient client = MinecraftClient.getInstance();
+			if (client.player instanceof ProjectileWeaponTimerUtils access) {
+				int ticks = access.templateProject$getHeldTicks();
+
+				if (ticks != 0) {
+					System.out.println(tickDelta);
+					float t = MathHelper.clamp(actualTicksElapsed / (25f - (5 * access.templateProject$getqcLevel())), 0f, 1f); // From 0 to 1
+
+					int armLength = 5;
+					int color = 0xFFFF0000;
+
+					int startPos = 5;
+
+					int tlX = MathHelper.lerp(t, centerX - startPos, centerX);
+					int tlY = MathHelper.lerp(t, centerY - startPos, centerY);
+
+					int tlDistX = tlX - centerX;
+					int tlDistY = tlY - centerY;
+
+					int trX = centerX - tlDistX;
+					int trY = tlY;
+
+					int blX = tlX;
+					int blY = centerY - tlDistY;
+
+					int brX = centerX - tlDistX;
+					int brY = centerY - tlDistY;
+
+					context.drawVerticalLine(tlX, tlY - armLength - 1, tlY, color);
+					context.drawHorizontalLine(tlX - armLength, tlX, tlY, color);
+
+					context.drawVerticalLine(trX, trY - armLength - 1, trY, color);
+					context.drawHorizontalLine(trX, trX + armLength, trY, color);
+
+					context.drawVerticalLine(blX, blY, blY + armLength + 1, color);
+					context.drawHorizontalLine(blX - armLength, blX, blY, color);
+
+					context.drawVerticalLine(brX, brY, brY + armLength + 1, color);
+					context.drawHorizontalLine(brX, brX + armLength, brY, color);
+				}
+			}
+		});
+
 	}
 
 }
